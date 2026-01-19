405cf540263a2e92a6b8eb82db72d319e53a1f80
Robert Schwartz <robschwartz@prettyapps.net>
2026-01-19 11:24:09 -0500

    layout nav tweak. db ctx namespace change

--- Data/BlazorTodoContext.cs
+++ Data/BlazorTodoContext.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 
+namespace BlazorTodo.Data;
+
 public class BlazorTodoContext(DbContextOptions<BlazorTodoContext> options) : DbContext(options)
 {
     public DbSet<BlazorTodo.Models.Movie> Movie { get; set; } = default!;
